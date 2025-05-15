251709b0fcfc90c0e8673f08fb1f42c67fb4fd1e
S2ej1n <ks320205@naver.com>
2025-05-15 16:34:38 +0900

    chore: styled component 글로벌 스타일 생성

--- frontend/lawn-garden/src/App.jsx
+++ frontend/lawn-garden/src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
-
-import './App.css'
+import styled from 'styled-components';
+import lawnLight from '@/assets/lawnLight.svg';
+import lawnDark from '@/assets/lawnDark.svg';
 
 function App() {
 
--- frontend/lawn-garden/src/main.jsx
+++ frontend/lawn-garden/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import GlobalStyle from '@/styles/GlobalStyle';
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { router } from '@/routes';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
