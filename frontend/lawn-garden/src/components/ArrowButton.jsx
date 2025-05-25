95bfe93c2a19d02692ab6a9e1910aea7c4a47b3c
S2ej1n <ks320205@naver.com>
2025-05-26 02:12:28 +0900

    design: 방향별 모양 설정

--- frontend/lawn-garden/src/components/ArrowButton.jsx
+++ frontend/lawn-garden/src/components/ArrowButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Button = styled.button`
     background-color: #997C70;
@@ -10,10 +10,24 @@ const Button = styled.button`
     padding: 1rem 2rem;
     font-size: 1.2rem;
     font-weight: bold;
+    border-radius: 10px;
 
     &:hover {
         background-color: #B89687;
     }
+
+    /* 방향 별 모양 조정 */
+    ${({direction}) => direction === 'left' && css`
+        clip-path: polygon(0 50%, 20% 0, 100% 0, 100% 100%, 20% 100%);
+        padding-left: 3rem;
+    `}
+    ${({direction}) => direction === 'right' && css`
+        clip-path: polygon(0 0, 80% 0, 100% 50%, 80% 100%, 0 100%);
+        padding-right: 3rem;
+    `}
+    ${({direction}) => direction === 'center' && css`
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    `}
 `
 export default function ArrowButton({direction, text}) {
   return (
